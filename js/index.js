d5e94b9783eed627c1bb60f6d4581996a35d68b4
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-16 16:25:17 +0100

    Add grid element

--- js/index.js
+++ js/index.js
@@ -7,4 +7,29 @@ const showNumber = () => {
     sliderValue.textContent = slider.value;
 }
 
-slider.addEventListener('mousemove', showNumber);
+slider.addEventListener('mousemove', changeGrid);
+
+function changeGrid() {
+    showNumber();
+    deleteContainerChilds();
+    for(let i = 0; i < slider.value; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        for(j = 0; j < slider.value; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            line.appendChild(pixel);
+        }
+        container.appendChild(line);
+    }
+}
+
+function deleteContainerChilds() {
+    let delChild = container.lastChild;
+    while(delChild) {
+        container.removeChild(delChild);
+        delChild = container.lastChild;
+    }
+}
+
+changeGrid()
