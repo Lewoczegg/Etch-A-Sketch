4183c65a28e199e6414a619d284fb9c24ad17486
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-17 18:05:08 +0100

    Add coloring grid

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 let container = document.querySelector('.container');
 
 let slider = document.querySelector('.slider');
@@ -18,6 +22,9 @@ function changeGrid() {
         for(j = 0; j < slider.value; j++) {
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
+            pixel.setAttribute('style', 'background: white');
+            pixel.addEventListener('mouseover', changeBackground);
+            pixel.addEventListener('mousedown', changeBackground);
             line.appendChild(pixel);
         }
         container.appendChild(line);
@@ -29,6 +36,24 @@ function deleteContainerChilds() {
     while(delChild) {
         container.removeChild(delChild);
         delChild = container.lastChild;
+    }
+}
+
+function changeBackground(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    const background = this.style.background;
+    console.log(this);
+    console.log(background);
+    if(background === 'white') {
+        this.setAttribute('style', 'background: rgb(204, 204, 204)');
+    } else if(background === 'rgb(204, 204, 204)') {
+        this.setAttribute('style', 'background: rgb(170, 170, 170)');
+    } else if (background === 'rgb(170, 170, 170)') {
+        this.setAttribute('style', 'background: rgb(100, 100, 100)');
+    } else if (background === 'rgb(100, 100, 100)') {
+        this.setAttribute('style', 'background: rgb(50, 50, 50)');
+    } else if (background === 'rgb(50, 50, 50)') {
+        this.setAttribute('style', 'background: black');
     }
 }
 
