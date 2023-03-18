63dc6a93b582a3e1949a5134d225972c31d76c16
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-18 17:04:21 +0100

    Fix bug with slider and fix bug with grid's pixels

--- js/index.js
+++ js/index.js
@@ -4,16 +4,18 @@ document.body.onmouseup = () => (mouseDown = false);
 
 let container = document.querySelector('.container');
 
-let slider = document.querySelector('.slider');
-let sliderValue = document.querySelector('#slider-value');
 
+let sliderValue = document.querySelector('#slider-value');
 const showNumber = () => {
     sliderValue.textContent = slider.value;
 }
 
+
+let slider = document.querySelector('.slider');
 slider.addEventListener('mousemove', changeGrid);
 
 function changeGrid() {
+    if(!mouseDown) return;
     showNumber();
     deleteContainerChilds();
     for(let i = 0; i < slider.value; i++) {
@@ -32,18 +34,12 @@ function changeGrid() {
 }
 
 function deleteContainerChilds() {
-    let delChild = container.lastChild;
-    while(delChild) {
-        container.removeChild(delChild);
-        delChild = container.lastChild;
-    }
+    container.innerHTML = '';
 }
 
 function changeBackground(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
     const background = this.style.background;
-    console.log(this);
-    console.log(background);
     if(background === 'white') {
         this.setAttribute('style', 'background: rgb(204, 204, 204)');
     } else if(background === 'rgb(204, 204, 204)') {
@@ -57,4 +53,21 @@ function changeBackground(e) {
     }
 }
 
-changeGrid()
+function init() {
+    sliderValue.textContent = 16;
+    for(let i = 0; i < slider.value; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        for(j = 0; j < slider.value; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.setAttribute('style', 'background: white');
+            pixel.addEventListener('mouseover', changeBackground);
+            pixel.addEventListener('mousedown', changeBackground);
+            line.appendChild(pixel);
+        }
+        container.appendChild(line);
+    }
+}
+
+init();
