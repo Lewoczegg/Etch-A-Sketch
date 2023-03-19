4cafa57fbfded52360c88d5cb2b7fddf0094f042
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-03-19 15:34:12 +0100

    Add rubber

--- js/index.js
+++ js/index.js
@@ -2,17 +2,17 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+let currentMode = 'pencil';
+
 let container = document.querySelector('.container');
-
-
+let slider = document.querySelector('.slider');
 let sliderValue = document.querySelector('#slider-value');
+
+slider.addEventListener('mousemove', changeGrid);
+
 function showNumber() {
     sliderValue.textContent = slider.value;
 }
-
-
-let slider = document.querySelector('.slider');
-slider.addEventListener('mousemove', changeGrid);
 
 function changeGrid() {
     if(!mouseDown) return;
@@ -39,18 +39,34 @@ function deleteContainerChilds() {
 
 function changeBackground(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
-    const background = this.style.background;
-    if(background === 'white') {
-        this.setAttribute('style', 'background: rgb(204, 204, 204)');
-    } else if(background === 'rgb(204, 204, 204)') {
-        this.setAttribute('style', 'background: rgb(170, 170, 170)');
-    } else if (background === 'rgb(170, 170, 170)') {
-        this.setAttribute('style', 'background: rgb(100, 100, 100)');
-    } else if (background === 'rgb(100, 100, 100)') {
-        this.setAttribute('style', 'background: rgb(50, 50, 50)');
-    } else if (background === 'rgb(50, 50, 50)') {
-        this.setAttribute('style', 'background: black');
+    if (currentMode === 'pencil') {
+        const background = this.style.background;
+        if(background === 'white') {
+            this.setAttribute('style', 'background: rgb(204, 204, 204)');
+        } else if(background === 'rgb(204, 204, 204)') {
+            this.setAttribute('style', 'background: rgb(170, 170, 170)');
+        } else if (background === 'rgb(170, 170, 170)') {
+            this.setAttribute('style', 'background: rgb(100, 100, 100)');
+        } else if (background === 'rgb(100, 100, 100)') {
+            this.setAttribute('style', 'background: rgb(50, 50, 50)');
+        } else if (background === 'rgb(50, 50, 50)') {
+            this.setAttribute('style', 'background: black');
+        }
+    } else if (currentMode === 'rubber') {
+        this.setAttribute('style', 'background: white');
     }
+}
+
+
+let tools = document.querySelectorAll('.tool');
+tools.forEach(tool => tool.addEventListener('click', toggleActiveMode))
+
+function toggleActiveMode() {
+    if (this.classList.contains('active')) return;
+    tools.forEach(tool => tool.classList.remove('active'));
+    this.classList.add('active');
+    currentMode = this.innerText.toLowerCase();
+    console.log(currentMode);
 }
 
 function init() {
